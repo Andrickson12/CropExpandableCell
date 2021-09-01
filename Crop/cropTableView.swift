d16f8bdc54096bacf1773707c3d5517832aaee9d
Andrickson12 <Andrickson12>
2021-09-01 17:52:56 -0400

    Expandable features.

--- Crop/cropTableView.swift
+++ Crop/cropTableView.swift
@@ -7,43 +7,75 @@
 
 import UIKit
 
+class Section {
+    let title: String
+    let options:[String]
+    var isOpened: Bool = false
+    
+    init(title: String, options: [String], isOpened: Bool = false) {
+        self.title = title
+        self.options = options
+        self.isOpened = isOpened
+    }
+}
+
 class cropTableView: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var segmentedControlBtn: UISegmentedControl!
-    
     //TableView
-    private let tableView: UITableView = {
+    private let tableViewData: UITableView = {
         let tableView = UITableView()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return tableView
     }()
     
-    //Hard-Coded tableView data
-    let tableContent = ["Areas of observation - please note concern(s) if any, as well as corrective action(s)","Surranding Areas/Adjacent Activities","Building Grounds","Building Structre","Water System", "Others"]
+    private var sections = [Section]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(tableView)
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.frame = view.bounds
+        sections = [
+            Section(title: "Areas of observation - please note concern(s) if any, as well as corrective action(s)", options: [0].compactMap({return "Cell \($0)"})),
+            Section(title: "Surranding Areas/Adjacent Activities", options: [0].compactMap({return "Cell \($0)"})),
+            Section(title: "Building Grounds", options: [0].compactMap({return "Cell \($0)"})),
+            Section(title: "Building Structre", options: [0].compactMap({return "Cell \($0)"})),
+            Section(title: "Water System", options: [0].compactMap({return "Cell \($0)"})),
+            Section(title: "Others", options: [0].compactMap({return "Cell \($0)"}))
+        ]
+        
+        view.addSubview(tableViewData)
+        tableViewData.delegate = self
+        tableViewData.dataSource = self
+        tableViewData.frame = view.bounds
         
         title = "Cooler Facility Risk Assessment"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.tintColor = UIColor.orange
-        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableContent.count
+        let section = sections[section]
+        
+        if section.isOpened {
+            return section.options.count + 1
+        } else {
+            return 1
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        //Globally
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = tableContent[indexPath.row]
         
+        if indexPath.row == 0 {
+            cell.textLabel?.text = sections[indexPath.section].title
+        } else {
+            cell.textLabel?.text = sections[indexPath.section].options[indexPath.row - 1]
+        }
         return cell
     }
     
@@ -53,6 +85,18 @@ class cropTableView: UIViewController, UITableViewDelegate, UITableViewDataSourc
         }
         else {
             cell.backgroundColor = .white
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            
+            sections[indexPath.section].isOpened = !sections[indexPath.section].isOpened
+            tableView.reloadSections([indexPath.section], with: .none)
+        }
+        else {
+            print("Press the UITableViewCell")
         }
     }
 }
